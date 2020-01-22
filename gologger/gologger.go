18aedbe27899df004dee755fc7b228cb8c554b59
Zawar-Ahmed10p <zawar.ahmed@tenpearls.com>
2020-01-22 16:41:43 +0500

    test

--- gologger/gologger.go
+++ gologger/gologger.go
@@ -13,23 +13,23 @@ import (
 
 var Logger *logrus.Logger
 
-// //GetLogger returns logger
-// func GetNewLogger() *logrus.Logger {
-// 	logger := logrus.New()
-// 	logger.SetOutput(&lumberjack.Logger{
-// 		Filename:   "err.log",
-// 		MaxSize:    1, //file size in megabytes
-// 		MaxBackups: 5, //max number of files
-// 		MaxAge:     2, //days
-// 	})
-// 	logger.SetFormatter(&easy.Formatter{
-// 		TimestampFormat: "2006-01-02 15:04:05",
-// 		LogFormat:       "%time% [%lvl%]: %msg% \n",
-// 	})
-// 	Logger = logger
-// 	return Logger
+//GetLogger returns logger
+func GetNewLogger() *logrus.Logger {
+	logger := logrus.New()
+	logger.SetOutput(&lumberjack.Logger{
+		Filename:   "err.log",
+		MaxSize:    1, //file size in megabytes
+		MaxBackups: 5, //max number of files
+		MaxAge:     2, //days
+	})
+	logger.SetFormatter(&easy.Formatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		LogFormat:       "%time% [%lvl%]: %msg% \n",
+	})
+	Logger = logger
+	return Logger
 
-// }
+}
 
 var initOnce sync.Once
 
